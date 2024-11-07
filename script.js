b263312276ec82b6cbbdfdfcaa3a8251c8408241
Taylor Bundy <martonbarbosa@hotmail.com>
2024-11-07 06:55:10 -0300

    Eddited - 4 - 07-11-2024

---
+++ script.js
@@ -0,0 +1,33 @@
+//const canvas = document.querySelector("InputContainer")
+
+//canvas.width = innerWidth
+//canvas.height = innerHeight
+
+//Inputs.width = innerWidth
+//Inputs.height = innerHeight
+
+//ff = document.getElementById("ff");
+//ca1 = document.getElementById("ca1");
+//ca2 = document.getElementById("ca2");
+//av = document.getElementById("av");
+
+/* function check() {
+    //if (document.getElementById("ff").checked == true)
+    if (ff.checked == true) {
+        ca1.checked = false
+        ca2.checked = false
+        av.checked = false
+    } else if (ca1.checked == true) {
+        ff.checked = false
+        ca2.checked = false
+        av.checked = false
+    } else if (ca2.checked == true) {
+        ff.checked = false
+        ca1.checked = false
+        av.checked = false
+    }
+    
+    //if (document.getElementById("ff").ch)
+        //ca1.check = false
+    //document.getElementById("ca1").checked = true
+} */
